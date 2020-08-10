7c9a24865f643e98fc6d26871d3ce82dd85c1389
garimahc15 <halcyongarima@gmail.com>
2020-08-10 22:52:21 +0530

    project work during  Dec2019-Jan2020

--- garima2/spectralclustermaster/spectralcluster/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from . import spectral_clusterer
-
-SpectralClusterer = spectral_clusterer.SpectralClusterer
-DEFAULT_REFINEMENT_SEQUENCE = spectral_clusterer.DEFAULT_REFINEMENT_SEQUENCE
